ed38c83408913592ff846d2b7f1daad267b22e9e
Ekwuno kosi <kosisochukwu13@yahoo.com>
2025-01-23 21:37:10 +0100

    started working

--- Client/src/pages/Pricing.tsx
+++ Client/src/pages/Pricing.tsx
@@ -1,7 +1,27 @@
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
+    <div>
+
+        <div className="bg-white">
+            <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-3xl mx-auto text-center">
+                <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">Simple, transparent pricing</h2>
+                <p className="mt-4 text-lg text-gray-500">No contracts, no hidden fees, cancel at any time.</p>
+            </div>
+            </div>
+        </div>
+    
+        <div className="bg-gray-50">
+            <div className="max-w-7xl mx-auto py-24 px-4 sm:px-6 lg:px-8">
+            <div className="sm:flex sm:flex-col sm:align-center">
+                <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl sm:leading-none">Pay as you go</h2>
+                <p className="mt-4 text-lg text-gray-500">No contracts, no hidden fees, cancel at any time.</p>
+                <p className="mt-4 text-lg text-gray-500">You only pay for what you use, and you can change or cancel your plan at any time.</p>
+                </div>
+                
+    </div>
+
   )
 }
 
